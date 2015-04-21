604882564efc3c7619ba1183080f0b5589796c05
Jonah Duckles <jduckles@ou.edu>
2015-04-21 15:54:32 -0500

    rejiggering

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,5 +2,8 @@
 git clone https://github.com/ouinformatics/restful-api-application-template client/
 git clone https://github.com/ouinformatics/restful-api api # -v /api /usr/src/app (for api container)
 mv -f api_config.py api/api/config.py
+git clone -b cookiecutter https://github.com/ouinformatics/risser-docker docker
+mv config/rabbitmq_config.sh docker/rabbitmq/config.sh
+
 docker/build.sh
 
--- hooks/pre_gen_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git clone -b cookiecutter https://github.com/ouinformatics/risser-docker docker
-docker/build.sh
-
