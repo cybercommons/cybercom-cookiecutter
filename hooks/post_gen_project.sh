97c002fc70f5644be1f401683e20362f8551d803
Jonah Duckles <jduckles@ou.edu>
2015-04-21 15:15:59 -0500

    all the dockers

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,3 +2,4 @@
 git clone https://github.com/ouinformatics/restful-api-application-template client/
 git clone https://github.com/ouinformatics/restful-api api # -v /api /usr/src/app (for api container)
 mv -f api_config.py api/api/config.py
+git clone https://github.com/ouinformatics/ccstack-docker docker
