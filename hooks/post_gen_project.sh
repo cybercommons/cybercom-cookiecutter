8fabd0268c4b66a1687dae1dbba65f6e1cce77db
Jonah Duckles <jduckles@ou.edu>
2015-04-21 16:24:43 -0500

    fixed recursive to risser-docker repo

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/ouinformatics/restful-api-application-template client/
 git clone https://github.com/ouinformatics/restful-api api # -v /api /usr/src/app (for api container)
 mv -f api_config.py api/api/config.py
-git clone -b cookiecutter https://github.com/ouinformatics/risser-docker docker
+git clone --recursive -b cookiecutter https://github.com/ouinformatics/risser-docker docker
 mv config/rabbitmq_config.sh docker/rabbitmq/config.sh
 
 docker/build.sh
