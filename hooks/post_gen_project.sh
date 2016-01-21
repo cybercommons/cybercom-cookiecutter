832986ccc4dc90f26891a117499f44ec6d4dcd19
Mark Stacy <mbstacy@gmail.com>
2016-01-21 10:36:40 -0600

    updated post hook to add portal data

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-git clone https://github.com/ouinformatics/restful-api-application-template client/
-git clone https://github.com/ouinformatics/restful-api api # -v /api /usr/src/app (for api container)
-mv -f api_config.py api/api/config.py
-git clone --recursive -b cookiecutter https://github.com/ouinformatics/risser-docker docker
-mv config/rabbitmq_config.sh docker/rabbitmq/config.sh
+git clone https://github.com/ouinformatics/restful-api-application-template data/static/portal
+git clone https://github.com/ouinformatics/restful-api api_code # -v /api /usr/src/app (for api container)
+#mv -f api_config.py api/api/config.py
+#git clone --recursive -b cookiecutter https://github.com/ouinformatics/risser-docker docker
+#mv config/rabbitmq_config.sh docker/rabbitmq/config.sh
 
-docker/build.sh
-
+#docker/build.sh
