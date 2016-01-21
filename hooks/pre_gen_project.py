40cd738c6ba2421eb563b0f7282d7bac4f76d8e1
Mark Stacy <mbstacy@gmail.com>
2016-01-21 12:03:45 -0600

    absolute path in script --debug

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,5 +7,5 @@ home = expanduser("~")
 current_dir =  os.getcwd() #os.path.dirname(os.path.realpath(__file__))
 print current_dir
 #module_name = '{{ cookiecutter.module_name }}'
-cookiecutter('risser-cookiecutter/',
+cookiecutter(os.path.join(home,'.cookiecutters/risser-cookiecutter/'),
              extra_context={'docker_host_data_directory':current_dir,'user_home':home})
