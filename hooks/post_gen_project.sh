c467115613e81c057b9fa26856864eb4e4998b92
Mark Stacy <mbstacy@gmail.com>
2016-02-22 22:59:00 -0600

    add docker build

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git clone https://github.com/ouinformatics/restful-api-application-template data/static/portal
 git clone https://github.com/ouinformatics/restful-api api_code # -v /api /usr/src/app (for api container)
+docker build -t api api_code/Dockerfile
 #mv -f api_config.py api/api/config.py
 #git clone --recursive -b cookiecutter https://github.com/ouinformatics/risser-docker docker
 #mv config/rabbitmq_config.sh docker/rabbitmq/config.sh
