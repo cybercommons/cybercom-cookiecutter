e9e0f4175ce1c38ea23ca7c45c5f560520fa2369
Mark Stacy <mbstacy@gmail.com>
2016-03-06 14:58:10 -0600

    updated celery mongo db backend - Error with host in settings needed to change to uri.

--- {{cookiecutter.application_short_name}}/celery/code/celeryconfig.py
+++ {{cookiecutter.application_short_name}}/celery/code/celeryconfig.py
@@ -2,9 +2,8 @@ BROKER_URL = 'amqp://{{cookiecutter.broker_user}}:{{cookiecutter.broker_pass}}@{
 CELERY_SEND_EVENTS = True
 CELERY_TASK_RESULT_EXPIRES = None
 CELERY_ACCEPT_CONTENT = ['pickle','json']
-CELERY_RESULT_BACKEND = "mongodb"
+CELERY_RESULT_BACKEND = "mongodb://{{cookiecutter.mongo_host}}:{{cookiecutter.mongo_port}}/"
 CELERY_MONGODB_BACKEND_SETTINGS = {
-    "host": "{{cookiecutter.mongo_host}}",
     "database": "{{cookiecutter.application_short_name}}",
     "taskmeta_collection": "tombstone"
 }
