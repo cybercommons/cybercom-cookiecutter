e4dcf2ffdfeede72cac6213490502731cbda115a
Mark Stacy <mbstacy@gmail.com>
2016-03-19 15:03:58 -0500

    updated git repos for api and portal

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git clone https://github.com/ouinformatics/restful-api-application-template data/static/portal
-git clone https://github.com/ouinformatics/restful-api api_code # -v /api /usr/src/app (for api container)
+git clone https://github.com/cybercommons/cybercom-portal data/static/portal
+git clone https://github.com/cybercommons/cybercom-api api_code # -v /api /usr/src/app (for api container)
 #docker build -t api api_code/Dockerfile
 #mv -f api_config.py api/api/config.py
 #git clone --recursive -b cookiecutter https://github.com/ouinformatics/risser-docker docker
