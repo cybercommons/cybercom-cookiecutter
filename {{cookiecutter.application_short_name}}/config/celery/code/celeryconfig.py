872adb0aaf736b276dba81241001587ed649d076
Mark Stacy <mbstacy@gmail.com>
2016-12-12 09:18:35 -0600

    removed and change cookiecutter questions and named connections in cybercom_up

--- {{cookiecutter.application_short_name}}/config/celery/code/celeryconfig.py
+++ {{cookiecutter.application_short_name}}/config/celery/code/celeryconfig.py
@@ -1,12 +1,10 @@
 import os
 appname = "{{cookiecutter.application_short_name}}"
-BROKER_URL = 'amqp://{{cookiecutter.broker_user}}:{{cookiecutter.broker_pass}}@{0}:{{cookiecutter.broker_port}}/{{cookiecutter.broker_vhost}}'
-BROKER_URL = BROKER_URL.format(os.environ["{0}_RABBITMQ_PORT_5672_TCP_ADDR".format(appname.upper())])
+BROKER_URL = 'amqp://{{cookiecutter.broker_user}}:{{cookiecutter.broker_pass}}@cybercom_rabbitmq:{{cookiecutter.broker_port}}/{{cookiecutter.broker_vhost}}'
 CELERY_SEND_EVENTS = True
 CELERY_TASK_RESULT_EXPIRES = None
 CELERY_ACCEPT_CONTENT = ['pickle','json']
-CELERY_RESULT_BACKEND = "mongodb://{0}:{{cookiecutter.mongo_port}}/"
-CELERY_RESULT_BACKEND =CELERY_RESULT_BACKEND.format(os.environ["{0}_MONGO_PORT_27017_TCP_ADDR".format(appname.upper())])
+CELERY_RESULT_BACKEND = "mongodb://cybercom_mongo:27017/"
 CELERY_MONGODB_BACKEND_SETTINGS = {
     "database": "{{cookiecutter.application_short_name}}",
     "taskmeta_collection": "tombstone"
