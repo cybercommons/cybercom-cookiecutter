458b834bb9b1a4d24d711000859f577f98acd40d
Tyler Pearson <tylerdpearson@gmail.com>
2017-01-03 09:54:24 -0600

    remove invalid template code

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,11 +4,3 @@ from os.path import expanduser
 
 home = expanduser("~")
 
-#current_dir =  os.getcwd() #os.path.dirname(os.path.realpath(__file__))
-#print(current_dir
-#cookiecutter.prompt.read_user_variable('docker_host_data_directory', current_dir)
-#cookiecutter.prompt.read_user_variable('user_home', home)
-#module_name = '{{ cookiecutter.module_name }}'
-#cookiecutter(os.path.join(home,'.cookiecutters/risser-cookiecutter/'),
-#             extra_context={'docker_host_data_directory':current_dir,'user_home':home})
-
