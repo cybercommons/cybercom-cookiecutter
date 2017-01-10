90dd007d39f3aa805ca6146778a082bbdee2e0c4
Tyler Pearson <tylerdpearson@gmail.com>
2017-01-10 11:41:56 -0600

    cleanup

--- {{cookiecutter.application_short_name}}/config/ssl/nginx/letsencrypt/entrypoint.sh
+++ {{cookiecutter.application_short_name}}/config/ssl/nginx/letsencrypt/entrypoint.sh
@@ -6,7 +6,7 @@ yum install certbot -y
 
 if [ ! -f /etc/letsencrypt/renewal ]
   then
-    certbot certonly --webroot -w /www -d cc.lib.ou.edu
+    certbot certonly --webroot -w /www -d {{cookiecutter.nginx_server_name}}
   else
     certbot renew --quiet
 fi
